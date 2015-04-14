7e3e199b6d2074b6931f022f40830523f0db8d7b
Kristi Nikolla <knikolla@bu.edu>
2015-04-14 15:03:29 -0400

    Hopefully final commit for fixing indentation. I think I converted all the hard tabs to spaces

--- scheduler/chris_scheduler.py
+++ scheduler/chris_scheduler.py
@@ -4,7 +4,10 @@ import sys
 import os
 import argparse
 
+
 class Scheduler(object):
+
+
     def __init__(self, host, remoteHost, remoteUser, runDir):
         self._remoteHost = remoteHost
         self._remoteUser = remoteUser
@@ -16,33 +19,38 @@ class Scheduler(object):
         self._channel = self._connection.channel()
         self._channel.queue_declare(queue='tasks', durable=True)
 
+
     def close(self):
         self._connection.close()
-    
+
+
     def send(self, command):
         self._channel.basic_publish(
             exchange = '',
             routing_key = 'tasks',
-            body = command, 
+            body = command,
             properties=pika.BasicProperties(
                 delivery_mode = 2))
+
 
     def readFile(self, filename):
         f = open(filename, 'r')
         command = f.read()
         return command
 
+
     def addPrefix(self, command):
         cmdPrefix = "echo 'Task Received'; " + \
-                'mkdir -p ' + self._chrisrunDir + '; ' + \
-                'scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ' + self._remoteUser + '@' + self._remoteHost + ':' + \
-                self._chrisrunDir + '/chris.env ' + self._chrisrunDir + '; ' + \
-                "echo 'export ENV_CLUSTERTYPE=crun' >> " + self._chrisrunDir + '/chris.env; ' + \
-                'scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ' + self._remoteUser + '@' + self._remoteHost + ':' + \
-                self._chrisrunDir + '/chris.run ' + self._chrisrunDir + ' && ' + \
+            'mkdir -p ' + self._chrisrunDir + '; ' + \
+            'scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ' + self._remoteUser + '@' + self._remoteHost + ':' + \
+            self._chrisrunDir + '/chris.env ' + self._chrisrunDir + '; ' + \
+            "echo 'export ENV_CLUSTERTYPE=crun' >> " + self._chrisrunDir + '/chris.env; ' + \
+            'scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ' + self._remoteUser + '@' + self._remoteHost + ':' + \
+            self._chrisrunDir + '/chris.run ' + self._chrisrunDir + ' && ' + \
 		"echo 'Task Completed'; "
         command = cmdPrefix + ' ' + command
         return command
+
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -50,14 +58,13 @@ if __name__ == '__main__':
 	parser.add_argument("-r", "--remotehost", help="remote host name")
 	parser.add_argument("-u", "--remoteuser", help="remote username")
 
-
 	exclusive_options = parser.add_mutually_exclusive_group()
 	exclusive_options.add_argument("-c", "--command", help="command to be executed")
 
 	args = parser.parse_args()
 
 	if not args.command:
-		sys.exit("error: missing command option")
+	   sys.exit("error: missing command option")
 
 	scheduler = Scheduler('localhost', args.remotehost, args.remoteuser, args.filepath)
 
